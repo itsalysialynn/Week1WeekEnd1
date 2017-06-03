1d6abec9efa6ab5381fa76ea97e60efe204e1f9e
Alysia Lynn <itsalysialynn@gmail.com>
2017-06-02 19:47:12 -0700

    fixed indentation

--- https_example.js
+++ https_example.js
@@ -1,20 +1,20 @@
-var https = require('https');
-console.log('I did it!');
+var https = require("https");
+console.log("I did it!");
 
 var options = {
-  host: 'stream-large-file.herokuapp.com',
-  path: '/give-me-stuff-now'
+  host: "stream-large-file.herokuapp.com",
+  path: "/give-me-stuff-now"
 };
 
 // called by https when the request is made.
 var callback = function(response) {
-  console.log('In response handler callback!');
+  console.log("In response handler callback!");
 
-  response.on('data', function(chunk) {
-    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+  response.on("data", function(chunk) {
+    console.log("[-- CHUNK OF LENGTH " + chunk.length + " --]");
     console.log(chunk.toString());
   });
-}
+};
 
 console.log("I'm about to make the request!");
 
